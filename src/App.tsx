ae5ab71fea6cf871d7639367e2d1ab4b328777a5
hasan <mmhotmail>
2023-10-04 16:03:33 +0300

    home

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritesPage from "./pages/FavoritesPage";
-import NavBar from "./components/NavBar";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import { Flip, ToastContainer, Zoom } from "react-toastify";
+import NavBar from "./components/Navbar";
 
 function App() {
   return (
